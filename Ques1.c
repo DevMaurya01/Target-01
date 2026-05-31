b97f6e80f2a2d80e967639c848ec3d3700f88712
DevMaurya01 <dev514416@gmial.com>
2026-05-31 16:23:18 +0530

    Add All files

---
+++ Ques1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void main()
+{
+    printf("Hello C! Programming");
+    printf("\n Hi How are you");
+    return;
+}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Target-01
 Welcome to C programming Target-01.
+<br>Target Complitation During ia 3 Months.
 <br> Author: Dev Maurya.
